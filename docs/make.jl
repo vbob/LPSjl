11799eae180475550066896ceb33797b6e2fe1c6
Vitor Barth <vitor.barth@gmail.com>
2022-03-23 22:42:21 -0400

    Docs: Removed comment on make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,9 +14,6 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
     repo = "github.com/vbob/LPSjl.jl.git"
 )
